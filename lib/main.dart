4c0bafe2802612314d15be5059ac2d66ab17c45e
crazytube2026 <info.sahad2002@gmail.com>
2026-08-25 03:27:53 +0600

    Update and rename main.dart to lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+wuimport 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
